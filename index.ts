adaf358e2e1d0bd8c84ec1ee20469c19be3e56ce
Hira Irfan <162793914+hirairfan04@users.noreply.github.com>
2024-05-07 20:26:18 +0500

    program files

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const answer: {
+    sentence: string
+} = await inquirer.prompt([
+{
+    name: "sentence",
+    type: "input",
+    message: "Enter a sentence or a paragraph to count words: "
+}
+])
+
+const words= answer.sentence.trim().split(" ")      // trim is used to remove white spaces
+// split is used to collect all the words in an array
+//console.log(words)
+console.log(`Your sentence contains ${words.length} words`)
